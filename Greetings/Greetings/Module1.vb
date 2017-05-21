e269ab9dae991e6cb7e2db991d7c0fcab9439bde
Brandon B <bmbost1983@gmail.com>
2017-05-21 14:50:44 -0500

    Add first project
    Added first project created for Chapter 2, using a console application to make a "Hello World"-type app.

---
+++ Greetings/Greetings/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Welcome to Visual Basic!")
+        Console.Write("Please type your name: ")
+        Dim name As String
+        name = Console.ReadLine()
+        Console.WriteLine("Hello, {0}!", name)
+        Console.WriteLine()
+        Console.WriteLine("Press Enter to quit...")
+        Console.ReadLine()
+    End Sub
+
+End Module
